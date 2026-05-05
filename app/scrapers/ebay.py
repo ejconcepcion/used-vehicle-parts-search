a80829b043e91b479054148149718af57b1fc262
ejc <ejconcepcion@gmail.com>
2026-05-05 00:04:12 -0700

    Add browser-like headers to eBay scraper to reduce 403s

--- app/scrapers/ebay.py
+++ app/scrapers/ebay.py
@@ -49,7 +49,16 @@ def fetch_sold_via_html(query: str) -> Tuple[float | None, int, list[float]]:
     )
     headers = {
         "User-Agent": config.USER_AGENT,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
     }
     log.info("eBay HTML query: %s", query)
     try:
